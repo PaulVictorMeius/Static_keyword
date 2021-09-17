483fc11bd552f10489b11dad1c8ae058796b0f26
Paul Victor Meius <paulmeius@yahoo.com>
2021-09-17 17:44:50 +0300

    Added an static const member

--- A.h
+++ A.h
@@ -5,7 +5,9 @@ class A
 public:
 	static int get_sim() { return sim; }
 	static void set_sim(int sim_) { sim = sim_; }
+	static int get_star(){ return star; }
 private:
 	static int sim;
+	static const int star = 5;
 };
 int A::sim = 7;
--- Static keyword.cpp
+++ Static keyword.cpp
@@ -4,5 +4,6 @@ int main()
     std::cout << "sim=" << A::get_sim() << '\n';
     A::set_sim(99);
     std::cout << "sim=" << A::get_sim() << '\n';
+    std::cout << "star=" << A::get_star() << '\n';
 }
 
